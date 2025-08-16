bee595a88cbdf6716779b613507b8137412430d1
Kili S <79143155+klinshy@users.noreply.github.com>
2025-08-16 22:04:05 +0200

    roofs and fog

--- src/misk-hq.ts
+++ src/misk-hq.ts
@@ -2,43 +2,53 @@
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-console.log('Script started successfully');
+console.log("Script started successfully");
 
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+WA.onInit()
+  .then(() => {
+    console.log("Scripting API ready");
+    console.log("Player tags: ", WA.player.tags);
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    WA.room.area.onEnter("clock").subscribe(() => {
+      const today = new Date();
+      const time = today.getHours() + ":" + today.getMinutes();
+      currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave("clock").subscribe(closePopup);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    bootstrapExtra()
+      .then(() => {
+        console.log("Scripting API Extra ready");
+      })
+      .catch((e) => console.error(e));
+  })
+  .catch((e) => console.error(e));
 
-}).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
+function closePopup() {
+  if (currentPopup !== undefined) {
+    currentPopup.close();
+    currentPopup = undefined;
+  }
 }
 WA.onInit().then(() => {
-WA.room.onEnterLayer('foreground/roof/roof_2').subscribe(() => {
-    WA.room.hideLayer('foreground/roof/roof_0');
-    WA.room.hideLayer('foreground/roof/roof_1');
-    WA.room.hideLayer('foreground/roof/roof_2');
-    WA.room.showLayer('background/floors/glasswall');
-});});
+  WA.room.onLeaveLayer("hideRoof").subscribe(() => {
+    WA.room.hideLayer("foreground/roof/roof_0");
+    WA.room.hideLayer("foreground/roof/roof_1");
+    WA.room.hideLayer("foreground/roof/roof_2");
+    WA.room.showLayer("background/floors/glasswall");
+  });
 
+  WA.room.onEnterLayer("hideRoof").subscribe(() => {
+    WA.room.showLayer("foreground/roof/roof_0");
+    WA.room.showLayer("foreground/roof/roof_1");
+    WA.room.showLayer("foreground/roof/roof_2");
+    WA.room.hideLayer("background/floors/glasswall");
+  });
+});
 
 export {};
